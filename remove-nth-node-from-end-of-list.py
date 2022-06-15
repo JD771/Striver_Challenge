200f8d58128360de5dbaad0f22c9c4c76174185d
Jahnavi Nandanwar <77223541+JD771@users.noreply.github.com>
2022-06-15 22:46:07 +0530

    Update remove-nth-node-from-end-of-list.py

--- remove-nth-node-from-end-of-list.py
+++ remove-nth-node-from-end-of-list.py
@@ -26,3 +26,22 @@ class Solution:
             curr = curr.next
             new_count +=1
         return head
+
+ # This solution will take the time complexity of O(n+n) i.e., O(2n) & the space complexity of O(1).
+# There is an optimized solution using two pointer approach.
+
+def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        fast, slow = head, head
+        for i in range(n):
+            fast = fast.next
+        
+        if not fast:
+            return head.next
+            
+        while fast.next:
+            slow = slow.next
+            fast = fast.next
+        slow.next = slow.next.next
+        return head
+
+# This solution will take time O(n) & space O(1).
