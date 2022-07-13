fd7ff6bc4f281413e69d9aaa9f1bc1ed5fcb5c56
Jahnavi Nandanwar <77223541+JD771@users.noreply.github.com>
2022-07-13 16:35:17 +0530

    Create implement-stack-using-queues.py

---
+++ implement-stack-using-queues.py
@@ -0,0 +1,20 @@
+class MyStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+
+    def pop(self) -> int:
+        return self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def empty(self) -> bool:
+        return len(self.stack)==0
+
+      
+# Time: O(1)
+# Space: O(1)
