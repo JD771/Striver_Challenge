13106903204a3af8563dd0106050b6b2266f69dc
Jahnavi Nandanwar <77223541+JD771@users.noreply.github.com>
2022-08-05 13:03:37 +0530

    Update binary-tree-inorder-traversal.py

--- binary-tree-inorder-traversal.py
+++ binary-tree-inorder-traversal.py
@@ -1,4 +1,4 @@
-# Approach 1
+# Approach 1: Using Recursion
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         ans = []
@@ -15,3 +15,42 @@ class Solution:
       
 # Time: O(n)
 # Space: O(n)
+
+
+# Approach 2: Using Stack
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        s, res = [], []
+        curr = root
+        while curr or s:
+            while curr:
+                s.append(curr)
+                curr = curr.left
+            curr = s.pop()
+            res.append(curr.val)
+            curr = curr.right
+        return res
+    
+# Time: O(n)
+# Space: O(n)
+
+# Approach 3: Morris Traversal- threaded Binary Tree
+ def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        curr = root
+        pre = None
+        while curr:
+            if not curr.left:
+                res.append(curr.val)
+                curr = curr.right
+            else:
+                pre = curr.left
+                while pre.right: pre = pre.right
+                pre.right = curr
+                temp = curr
+                curr = curr.left
+                temp.left = None
+        return res
+    
+# Time: O(n)
+# Space: O(1)
